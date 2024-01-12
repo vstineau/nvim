6c4b98118174bba7a7956a05c2a25c6b67abc4c2
Vincent Stineau <vstineau@bess-f2r4s9.clusters.42paris.fr>
2024-01-12 19:32:24 +0100

    lsp intalled

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 
 return {
+	{
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
@@ -9,5 +10,20 @@ return {
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 	end
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup ({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown {
+						}
+					}
+				}
+			})
+			require("telescope").load_extension("ui-select")
+			end
+	}
 }
 

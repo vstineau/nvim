aff32569b5fbbd97677f80b9749ae83c11084861
Vincent Stineau <vstineau@bess-f4r3s2.clusters.42paris.fr>
2024-01-15 13:39:58 +0100

    mapping

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,3 +9,9 @@ vim.cmd("set wildmenu")
 vim.cmd("set nocompatible")
 vim.cmd("set mouse=a")
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>h", "<C-W>h", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>j", "<C-W>j", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>k", "<C-W>k", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>l", "<C-W>l", { noremap = true })

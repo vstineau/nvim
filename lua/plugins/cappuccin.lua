8e43ae3f293faf5fa72002f0196a5df82c2fd528
Vincent Stineau <vstineau@bess-f1r3s3.clusters.42paris.fr>
2024-03-08 15:48:18 +0100

    background dark via catpuccin

--- lua/plugins/cappuccin.lua
+++ lua/plugins/cappuccin.lua
@@ -1,9 +1,44 @@
-
-return { 
+return {
 	"catppuccin/nvim",
 	name = "catppuccin",
-	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme "catppuccin-mocha"
-	end
+		require("catppuccin").setup {
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			term_colors = true,
+			transparent_background = false,
+			no_italic = false,
+			no_bold = false,
+			styles = {
+				comments = {},
+				conditionals = {},
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+			},
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
+				},
+			},
+			highlight_overrides = {
+				mocha = function(C)
+					return {
+						TabLineSel = { bg = C.pink },
+						CmpBorder = { fg = C.surface2 },
+						Pmenu = { bg = C.none },
+					}
+				end,
+			},
+		}
+
+		vim.cmd.colorscheme "catppuccin"
+	end,
 }

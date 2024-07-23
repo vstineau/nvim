6996a4f8c0f68355b7409d1f45cc4801cfc47489
Vincent Stineau <vstineau@bess-f2r1s10.clusters.42paris.fr>
2024-07-23 13:35:25 +0200

    mason fix

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,6 +8,7 @@ return {
  	{
  		'williamboman/mason-lspconfig.nvim',
  		config = function()
+			require("mason").setup()
  			require("mason-lspconfig").setup({
  				ensure_installed = { "lua_ls", "clangd", "bashls", "pyright", "tsserver", "rust_analyzer" },
  			})

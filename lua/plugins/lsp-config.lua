f1c140f1d928abd0d9af6dd6801c5959b812fbb7
Vincent Stineau <vstineau@bess-f2r1s10.clusters.42paris.fr>
2024-07-23 14:02:54 +0200

    leader rn pour changer nom variable

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -34,6 +34,7 @@ return {
  			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
  			vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
  			vim.keymap.set({ 'n' }, '<leader>e', vim.diagnostic.open_float, {})
+			vim.keymap.set({ 'n' }, '<leader>rn', vim.lsp.buf.rename, {})
 
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup('UserLspConfig', {}),

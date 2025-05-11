c89912a4d704a1b45fd95d4c583cfd841da9a958
vstineau <vstineau@student.42.fr>
2025-05-11 19:24:03 +0200

    map telescope lsp document symbols on leader fs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,7 @@ return {
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+		vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 	end
 	},
